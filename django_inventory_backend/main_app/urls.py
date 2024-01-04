9a0d4302c4ae1a5977f5a7adb4fdf0f28adbfda7
achasek <chaseknieper@gmail.com>
2024-01-04 12:21:46 -0700

    testing rename

--- django_inventory_backend/main_app/urls.py
+++ django_inventory_backend/main_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.base_url, name='base'),  
     path('items/', views.items_index, name='items_index'),
+    # old way
     # path('items/create/', views.ItemCreate.as_view(), name='items_create'),
     # new way of views with rest api
     path('items/<int:pk>', views.items_detail, name="items_detail"),
